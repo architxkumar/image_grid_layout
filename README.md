96ed47dd203008fca1abfab655ae965763594cc5
Archit Kumar <architxkumar@proton.me>
2025-10-11 18:35:08 +0530

    chore: initial project setup

---
+++ README.md
@@ -0,0 +1,4 @@
+# image_grid_layout
+
+A flutter project to understand about grid in flutter
+
---
+++ android/app/src/main/kotlin/com/example/imagegridlayout/image_grid_layout/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.imagegridlayout.image_grid_layout
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
